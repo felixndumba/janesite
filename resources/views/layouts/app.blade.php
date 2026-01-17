3dbd6e8f53fd8067efe1349ae62bbe266be5705e
felixndumba <127409107+felixndumba@users.noreply.github.com>
2026-01-17 13:04:41 +0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
+        <meta name="google-site-verification" content="kVKfXrtuFEhvvqHyS6zuI5okOHNvPxS-DTcKi4yHXKM" />
         <title>{{ config('app.name', 'janendichu') }}</title>
 
         <!-- Fonts -->
